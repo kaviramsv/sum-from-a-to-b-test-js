a2aa2a08040b15db8b909f716e9a4fd2d98e7e6d
kaviramsv <kavitasvenkitachalam@gmail.com>
2021-10-30 12:59:52 +0000

    recursive a,b

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
-}
+  let total =fromN;
 
+  if (fromN<toN) {
+    // console.log(fromN,toN,total);
+    total=total+sum(fromN+1,toN);
+  }
+  //console.log("final",total);
+  return total;
+
+}
+sum(3, 7);
 module.exports = sum;
